1078c2c6859330322ecf01b44f94a76af3f7a8d7
veronika.qwest@gmail.co <u0060@headnode.imkn>
2018-06-05 12:23:41 +0500

    changes

--- DREAM/src/strategy/stitching/branch_train.py
+++ DREAM/src/strategy/stitching/branch_train.py
@@ -7,12 +7,12 @@ import torch
 import torch.nn as nn
 from branch_util import *
 
-from DREAM.src.strategy.stitching.branch_util import Branch
+from branch_util import Branch
 
 parser = argparse.ArgumentParser(description='Pytorch Branch Finetuning')
-parser.add_argument('-ilf', '--image-list-file', default='../../stitching/sample_img_list.txt',
+parser.add_argument('-ilf', '--image-list-file', default='/home/u0060/Profile-Persons-Identification/DREAM/data/stitching/sample_img_list.txt',
                     type=str, metavar='N', help='image list file')
-parser.add_argument('-ff', '--feat-file', default='../../stitching/ext_feat.bin',
+parser.add_argument('-ff', '--feat-file', default='/home/u0060/Profile-Persons-Identification/DREAM/data/stitching/ext_feat.bin',
                     type=str, metavar='N', help='extracted feature')
 parser.add_argument('-b', '--batch-size', default=256, type=int, metavar='N', help='mini-batch size (default: 256)')
 parser.add_argument('-f', '--feat-len', default=256, type=int, metavar='F', help='feature length (default: 256)')
@@ -31,7 +31,6 @@ def main():
     feat_file = args.feat_file
 
     train_map = get_dict(img_list_file)
-
     data = load_feat(feat_file)
     data = np.vstack(data)
 
