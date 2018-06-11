3ff20120a318b321c1bc5d1daf197af245c99dda
veronika.qwest@gmail.co <u0060@headnode.imkn>
2018-06-11 19:04:32 +0500

    add

--- Utils/pose_estimation/main.py
+++ Utils/pose_estimation/main.py
@@ -13,31 +13,28 @@ parser.add_argument('--path', metavar='DIR', default='', help='path to folder')
 # Read all jpg images in folder.
 def readImages(path):
     # Create array of array of images.
-    imagesArray = [];
+    imagesArray = dict();
 
     # List all files in the directory and read points from text files one by one
     for filePath in os.listdir(path):
         if filePath.endswith(".jpg"):
             img_path = os.path.join(path, filePath)
             img = cv2.imread(img_path);
-
-            # Convert to floating point
-            imagesArray.append((filePath),(img));
+            imagesArray[filePath] = img;
 
     return imagesArray;
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    path = os.getcwd() #args.path
+    path = args.path
 
-    images = readImages(path);
-    print(images)
+    images_dict = readImages(path);
     w_out = 300;
     h_out = 300;
 
-    for file, img in images:
-        #выровнять
-        align_img = align(img, w_out, h_out, file)
-        #найти углы
+    for file, img in images_dict.items():
+        print(file)
+        print(detect_landmarks(img))
+        #align_img = align(img, w_out, h_out, file)
         #angle = get_angle(align_img, file)
