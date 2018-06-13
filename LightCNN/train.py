66c9600394cf422bf5a5647c69e5146d68654054
snakeoniver <veronika.qwest@gmail.com>
2018-06-13 15:43:32 +0500

    commit

--- LightCNN/train.py
+++ LightCNN/train.py
@@ -16,23 +16,28 @@ import torch.utils.data
 def train(train_loader, model, criterion, optimizer, epoch, print_freq):
     batch_time = AverageMeter()
     data_time = AverageMeter()
+    cla_losses = AverageMeter()
+    yaw_losses = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
     top5 = AverageMeter()
 
+    # switch to train mode
     model.train()
 
     end = time.time()
-    for i, (input, target) in enumerate(train_loader):
-        data_time.update(time.time() - end)
+    for i, (input, target, yaw) in enumerate(train_loader):
+        data_time.update(time.time() - end) # measure data loading time
 
-        input = input.cuda()
         target = target.cuda()
+        yaw = yaw.float().cuda(async=True)
+        input = input.cuda()
         input_var = torch.autograd.Variable(input)
+        yaw_var = torch.autograd.Variable(yaw)
         target_var = torch.autograd.Variable(target)
 
         # compute output
-        output, _ = model(input_var)
+        output, _ = model(input_var, yaw_var)
         loss = criterion(output, target_var)
 
         # measure accuracy and record loss
@@ -71,14 +76,16 @@ def validate(val_loader, model, criterion):
     model.eval()
 
     end = time.time()
-    for i, (input, target) in enumerate(val_loader):
+    for i, (input, target, yaw) in enumerate(val_loader):
         input = input.cuda()
         target = target.cuda()
+        yaw = yaw.float().cuda(async=True)
         input_var = torch.autograd.Variable(input, volatile=True)
         target_var = torch.autograd.Variable(target, volatile=True)
+        yaw_var = torch.autograd.Variable(yaw)
 
         # compute output
-        output, _ = model(input_var)
+        output, _ = model(input_var, yaw_var)
         loss = criterion(output, target_var)
 
         # measure accuracy and record loss
@@ -87,13 +94,28 @@ def validate(val_loader, model, criterion):
         top1.update(prec1[0], input.size(0))
         top5.update(prec5[0], input.size(0))
 
+    # measure elapsed time
+    batch_time.update(time.time() - end)
+    end = time.time()
+
     print('\nTest set: Average loss: {}, Accuracy: ({})\n'.format(losses.avg, top1.avg))
+
+    print('Test: [{0}/{1}]\t'
+          'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+          'Average Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+          'Prec@1(Accuracy) {top1.val:.3f} ({top1.avg:.3f})\t'
+          'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+        i, len(val_loader), batch_time=batch_time, loss=losses,
+        top1=top1, top5=top5))
+
+    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'.format(top1=top1, top5=top5))
 
     return top1.avg
 
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
+
     def __init__(self):
         self.reset()
 
