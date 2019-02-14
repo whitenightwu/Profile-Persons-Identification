89945f633f9efe4185dcb67086ce8453aae8ff5f
ydwu <ydwu@virgo.com>
2019-02-14 15:40:53 +0800

    modify 'img_preprocess/align_cfp.sh' and run it

--- Benchmarks/CFP_benchmark/img_preprocess/align_cfp.sh
+++ Benchmarks/CFP_benchmark/img_preprocess/align_cfp.sh
@@ -1,17 +1,17 @@
 # prepare CFP data first
-dst_dir=../../data/CFP
-list_file_name=/mnt/SSD/rongyu/data/cfp/cfp_dataset/Data/list_name.txt
-dst_file_name=white-align_img_list.txt
+dst_dir=/home/ydwu/tmp/gen_bin/cfp-dst
+list_file_name=/home/ydwu/tmp/gen_bin/cfp/Data/list_name.txt
+dst_file_name=/home/ydwu/tmp/gen_bin/white-align_img_list.txt
 python pre_cfp_data.py $dst_dir $list_file_name $dst_file_name
 
 
-# align CFP image
-pnp_file=pnp.txt
-image_prefix=/home/u0060/Datasets/cfp-dataset/
-alignment_file=cfp_alignment.txt
-aligned_img_file=align_img_list.txt
-pose_file=estimate_pose.txt
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/u0060/anaconda3/envs/py36/lib
-./test_process_align $pnp_file $image_prefix $alignment_file $aligned_img_file $pose_file
+# # align CFP image
+# pnp_file=pnp.txt
+# image_prefix=/home/ydwu/tmp/gen_bin/cfp-dst/
+# alignment_file=cfp_alignment.txt
+# aligned_img_file=align_img_list.txt
+# pose_file=estimate_pose.txt
+#
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/u0060/anaconda3/envs/py36/lib
+#./test_process_align $pnp_file $image_prefix $alignment_file $aligned_img_file $pose_file
