e5c03a4efb53462107b91c33aca7df2ef7f4a80e
ydwu <ydwu@virgo.com>
2019-02-18 16:24:27 +0800

    add 'head_pose_estimation_cnn'

---
+++ Benchmarks/CFP_benchmark/white_preprocess/ex_cnn_head_pose_estimation_images.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+#The MIT License (MIT)
+#Copyright (c) 2016 Massimiliano Patacchiola
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+#CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+#SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import tensorflow as tf
+import cv2
+from deepgaze.head_pose_estimation import CnnHeadPoseEstimator
+
+sess = tf.Session() #Launch the graph in a session.
+my_head_pose_estimator = CnnHeadPoseEstimator(sess) #Head pose estimation object
+
+# Load the weights from the configuration folders
+my_head_pose_estimator.load_roll_variables("roll/cnn_cccdd_30k.tf")
+my_head_pose_estimator.load_pitch_variables("pitch/cnn_cccdd_30k.tf")
+my_head_pose_estimator.load_yaw_variables("yaw/cnn_cccdd_30k.tf")
+
+
+for i in range(1,9):
+    file_name = "/home/ydwu/project3/zihui_DREAM/preprocess/single-img/" + str(i) + ".jpg"
+    print("Processing image ..... " + file_name)
+    image = cv2.imread(file_name) #Read the image with OpenCV
+
+    # yaw = my_head_pose_estimator.return_yaw(image) #Evaluate the yaw angle using a CNN
+    # yaw = my_head_pose_estimator.return_yaw(image, radians=False)
+
+    # Get the angles for roll, pitch and yaw
+    roll = my_head_pose_estimator.return_roll(image)  # Evaluate the roll angle using a CNN
+    pitch = my_head_pose_estimator.return_pitch(image)  # Evaluate the pitch angle using a CNN
+    yaw = my_head_pose_estimator.return_yaw(image)  # Evaluate the yaw angle using a CNN
+    print("Estimated [roll, pitch, yaw] ..... [" + str(roll[0,0,0]) + "," + str(pitch[0,0,0]) + "," + str(yaw[0,0,0])  + "]")
